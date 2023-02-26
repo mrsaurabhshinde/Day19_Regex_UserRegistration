91fc942820a7b470cef0443e0ef4a22d767c0936
saurabh <mrsaurabhshinde5@gmail.com>
2023-02-26 21:25:18 +0530

    Last Name Validation

--- src/com/bridgelabz/ValidationRegistration.java
+++ src/com/bridgelabz/ValidationRegistration.java
@@ -1,6 +1,7 @@
 package com.bridgelabz;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ValidationRegistration {
@@ -14,11 +15,24 @@ Scanner sc = new Scanner(System.in);
           System.out.println("The first name is invalid");
       }
     }
+
+    public void lastNameValidation(){
+        System.out.print("Enter a Last name: ");
+        String lastname = sc.nextLine();
+        if(Pattern.matches("^[A-Z]{1}[a-z]{2,}", lastname)){
+            System.out.println("The Last name is valid");
+        }else{
+            System.out.println("The Last name is invalid");
+        }
+    }
+
+
 }
 
  class UserRegistrationMain{
      public static void main(String[] args) {
     ValidationRegistration validate = new ValidationRegistration();
     validate.firstNameValidation();
+    validate.lastNameValidation();
      }
     }
